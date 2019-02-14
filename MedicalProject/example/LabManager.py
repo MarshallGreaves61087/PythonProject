381d60e5a839e0d93d5a329e2bbe772d4662274b
MarshallGreaves61087 <44975131+MarshallGreaves61087@users.noreply.github.com>
2019-02-14 13:24:42 +0000

    ut

--- MedicalProject/example/LabManager.py
+++ MedicalProject/example/LabManager.py
@@ -16,31 +16,34 @@ class Lab_Manager_data(object):
      
     def __init__(self,params):
         self.name=params["name"]
-        self.test=int(params["test"])
+        self.location=params["location"]
+        self.test=params["test"]
         self.result=params["result"]
 
 class Lab_Manager(db.Model):
     
-    __tablename__="Lab_Manager"
+    __tablename__="Lab_Manager_2"
     lab_manager_id = db.Column(db.Integer,primary_key=True)
     name = db.Column('lab_manager_name',db.String(50))
+    location = db.Column('lab_manager_location',db.String(50))
     test = db.Column('lab_manager_test',db.String(50))
     result = db.Column('lab_manager_result',db.String(50))
     
     def __init__(self,params):
         self.name=params["name"]
+        self.location=params["location"]
         self.test=params["test"]
         self.result=params["result"]
         pass
             
 @app.route('/api/labmanager')
 def example_Lab_Manager():
-    lm = Lab_Manager({"name":"Dr Farquad","test":"XRay","result":"Broken Forearm"})
+    lm = Lab_Manager({"name":"Dr Farquad","location":"Bristol","test":"XRay","result":"Broken Forearm"})
     db.session.add(lm)
     db.session.commit()
     labManager = Lab_Manager.query.all()
     for lm in labManager:
-        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Test: ",
+        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Location: ",lm.location,"Test: ",
               lm.test,"Result: ",lm.result)
         
     return str(labManager)    
@@ -48,12 +51,13 @@ def example_Lab_Manager():
 @app.route('/api/labmanager/register',methods=['POST'])
 def insert_Lab_Manager():   
     lm = db.session.add(Lab_Manager({"name":request.form.get("name"),
-                                "test":request.form.get("test"),
-                                "result":request.form.get("result")}))
+                                     "location":request.form.get("location"),
+                                     "test":request.form.get("test"),
+                                     "result":request.form.get("result")}))
     db.session.commit()
     labManager = Lab_Manager.query.all()
     for lm in labManager:
-        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Test: ",
+        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Location: ",lm.location,"Test: ",
               lm.test,"Result: ",lm.result)
         
     return str(labManager)    
@@ -61,6 +65,13 @@ def insert_Lab_Manager():
 @app.route('/api/labmanager/list')
 def fetch_Reports():
     return jsonpickle.encode(Lab_Manager.query.all())
+
+@app.route('/api/labmanager/delete/<int:lab_manager_id>', methods = ['DELETE'])
+def delete_lab_manager(lab_manager_id):
+    labmanager = Lab_Manager.query.get(int(lab_manager_id))
+    db.session.delete(labmanager)
+    db.session.commit()
+    return jsonpickle.encode(labmanager)
 
 if __name__ == '__main__':
     #db.create_all()    #creates database
