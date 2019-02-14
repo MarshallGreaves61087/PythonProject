7480e547ce6db16ca15485df55e4955ca49d7e6c
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-02-14 16:54:15 +0000

    commiting changes

--- MedicalProject/features/pd.feature
+++ MedicalProject/features/pd.feature
@@ -4,9 +4,4 @@ Scenario: Load Patient in Database Table
 	Given Request for all patients
 	Then Have all the Patients from the API
 	
-Scenario: Fetch All Patient Details from the database Table
-	Given Querying for Patient Data from Table
-	|name	|age	|address		|email			|gender |
-	|Rob	|25		|New Address 5	|rob@rob.com	|Male	|
-	|Clint	|70		|New Address 6	|clint@clint.com|Male	|
-	Then increases Patient Count from API	
+	
--- MedicalProject/features/steps/FlaskApplicationSteps.py
+++ MedicalProject/features/steps/FlaskApplicationSteps.py
@@ -5,27 +5,19 @@ from selenium import webdriver
 
 @given("Request for all patients")
 def fecth_patients_from_api(context):
-    context.patients=requests.get("localhost:7700/api/patients/list").json()
+    context.patients=requests.get("http://localhost:7770/api/patients/list").json()
     
 @then("Have all the Patients from the API")
 def check_all_patients_are_present(context):
     ok_(len(context.patients)>0,"Patients not Found")
+        
+
+@given("Request for all Report")
+def fecth_reports_from_api(context):
+    context.reports=requests.get("http://localhost:7770/api/reports/list").json()
     
-@given("Querying for Patient Data from Table")
-def post_patients_to_api(context):
-    context.currentCount = len(requests.get(
-        "localhost:7700/api/patients/list").json())
-    for row in context.table:
-        new_patient = requests.post("localhost:7700/api/patients/register",
-                                    data={"name":row["name"],
-                                          "age":row["age"],
-                                          "address":row["address"],
-                                          "email":row["email"],
-                                          "gender":row["gender"]})
+@then("Have all the Report from the API")
+def check_all_reports_are_present(context):
+    ok_(len(context.reports)>0,"Reports not Found")
     
-    print(new_patient)
     
-@then("increases Patient Count from API")
-def check_count_increase(context):
-    ok_(context.currentCount<len(requests.get(
-        "ocalhost:7700/api/patients/list").json()),"Patient Registration Failed")
