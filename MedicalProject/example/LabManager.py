5a9ceb0a15b823f9a52641851260b6e8c2e6aacf
61082EleanorBound <44974866+61082EleanorBound@users.noreply.github.com>
2019-02-14 13:05:59 +0000

    pushing association stuff

--- MedicalProject/example/LabManager.py
+++ MedicalProject/example/LabManager.py
@@ -9,7 +9,7 @@ import jsonpickle
 from flask.globals import request
 
 app= Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/graduate_training'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/medical_data'
 db = SQLAlchemy(app)
 
 class Lab_Manager_data(object):
@@ -21,11 +21,14 @@ class Lab_Manager_data(object):
 
 class Lab_Manager(db.Model):
     
-    __tablename__="Lab_Manager"
+    __tablename__="alc_Lab_Manager"
     lab_manager_id = db.Column(db.Integer,primary_key=True)
     name = db.Column('lab_manager_name',db.String(50))
     test = db.Column('lab_manager_test',db.String(50))
     result = db.Column('lab_manager_result',db.String(50))
+    
+    reports = db.relationship('Report',
+                                         backref=db.backref('lab_manager'),lazy=True)
     
     def __init__(self,params):
         self.name=params["name"]
@@ -63,8 +66,8 @@ def fetch_Reports():
     return jsonpickle.encode(Lab_Manager.query.all())
 
 if __name__ == '__main__':
-    #db.create_all()    #creates database
-    #example_Lab_Manager()
+    db.create_all()    #creates database
+    example_Lab_Manager()
     
     app.run(port=7700)
     pass
--- MedicalProject/example/MedicalApplication.py
+++ MedicalProject/example/MedicalApplication.py
@@ -29,10 +29,16 @@ class Patient(db.Model):
     address = db.Column('patient_address',db.String(50))
     email = db.Column('patient_email',db.String(50))
     gender = db.Column('gender',db.String(10))
+    
     #one patient has many reports
     reports = db.relationship('Report',
                                          backref=db.backref('patient'),lazy=True)
     
+    #many patients for one lab manager
+    lab_manager_id = db.Column(db.Integer,
+                           db.ForeignKey('alc_Lab_Manager.lab_manager_id'),
+                           nullable=False)
+
     def __init__(self,params):
         #self.patient_id = int(params["patient_id"])
         self.name=params["name"]
@@ -51,24 +57,57 @@ class Report(db.Model):
     related_illness=db.Column('related_illness',db.String(50))
     date=db.Column('date',db.String(50))
     notes = db.Column('notes',db.String(300))
-    perscription = db.Column('perscription',db.String(20))
+    perscription = db.Column('prescription',db.String(20))
     
     #many reports for one patient
     patient_id = db.Column(db.Integer,
-                           db.ForeignKey('alc_Patients.patient_id'),nullable=False)
+                           db.ForeignKey('alc_Patients.patient_id'),
+                           nullable=False)
+    
+    #many reports for one lab manager
+    #lab_manager_id = db.Column(db.Integer,
+    #                       db.ForeignKey('alc_Lab_Manager.lab_manager_id'),
+    #                       nullable=False)
     
     def __init__(self,params):
         self.title=params["title"]
         self.related_illness=params["related_illness"]
         self.date=params["date"]
         self.notes=params["notes"]
-        self.perscription=params["perscription"]   
+        self.perscription=params["perscription"]
+
+
+class Lab_Manager(db.Model):
+    
+    __tablename__="alc_Lab_Manager"
+    lab_manager_id = db.Column(db.Integer,primary_key=True)
+    name = db.Column('lab_manager_name',db.String(50))
+    test = db.Column('lab_manager_test',db.String(50))
+    result = db.Column('lab_manager_result',db.String(50))
+
+    #one lab manager has many reports
+    #reports = db.relationship('Report',
+    #                                     backref=db.backref('labManager'),lazy=True)
+    #one lab manager has many patients
+    patients = db.relationship('Patient',
+                                         backref=db.backref('labManager'),lazy=True)
+    
+    def __init__(self,params):
+        self.name=params["name"]
+        self.test=params["test"]
+        self.result=params["result"]
+        pass   
             
 @app.route('/patients')
 def example_Patient():
     p = Patient({"name":"Example 3","age":55,"address":"New Address 3","email":"example@hotmail.com",
                  "gender":"Male"})
+    l = Lab_Manager({"name":"Dr Jones","test":"MRA","result":"Clear"})
+    
+    l.patients.append(p)
     db.session.add(p)
+    db.session.add(l)
+    
     db.session.commit()
     patients = Patient.query.all()
     for p in patients:
@@ -81,23 +120,38 @@ def example_Patient():
 def example_Report():
     r = Report({"title":"Report 3","related_illness":"illness 3","date":"11.11.2018",
                 "notes":"note example 3","perscription":"example 3"})
+    
     p = Patient({"name":"Patient with Report","age":25,"address":"Patient Address",
                  "email":"example@gmail.com","gender":"Male"})
+    
+    #l = Lab_Manager({"name":"Dr Steve","test":"Blood Test","result":"Clear"})
+    
     p.reports.append(r)
+    #l.reports.append(r)
     
     db.session.add(r)
     db.session.add(p)
+    #db.session.add(l)
+    
     db.session.commit()
     reports = Report.query.all()
     for r in reports:
         print("Id: ",r.report_id,"Title: ",r.title,"Related Illness: ",r.related_illness,
-              "Date: ",r.date,"Notes: ",r.notes,"Perscription: ",r.perscription)
-    
-#    p = Patient({"name":"Patient with Report","age":25,"address":"Patient Address",
-#                 "email":"example@gmail.com","gender":"Male"})
-#    p.reports.append(r) #associating patient details with report data
-    
-    return str(reports)    
+              "Date: ",r.date,"Notes: ",r.notes,"Prescription: ",r.perscription)
+        
+    return str(reports)
+
+@app.route('/api/labmanager')
+def example_Lab_Manager():
+    lm = Lab_Manager({"name":"Dr Farquad","test":"XRay","result":"Broken Forearm"})
+    db.session.add(lm)
+    db.session.commit()
+    labManager = Lab_Manager.query.all()
+    for lm in labManager:
+        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Test: ",
+              lm.test,"Result: ",lm.result)
+        
+    return str(labManager)      
 
 @app.route('/api/patients/register',methods=['POST'])
 def insert_Patient():
@@ -130,12 +184,16 @@ def insert_Report():
                                "related_illness":request.form.get("related_illness"),
                                "date":request.form.get("date"),
                                "notes":request.form.get("notes"),
-                               "perscription":request.form.get("perscription")}))
+                               "perscription":request.form.get("perscription"),
+                               "patient_id":request.form.get("patient_id")}))
+    
+    p = Patient.query.filter_by(patient_id=request.form.get('patient_id')).first()
+    p.reports.append(r)
     db.session.commit()
     reports = Report.query.all()
     for r in reports:
         print("Id: ",r.report_id,"Title: ",r.title,"Related Illness: ",r.related_illness,
-              "Date: ",r.date,"Notes: ",r.notes,"Perscription: ",r.perscription)
+              "Date: ",r.date,"Notes: ",r.notes,"Perscription: ",r.perscription,"Patient Id: ",r.patient_id)
     return str(reports) 
 
 @app.route('/web/reports/register',methods=['POST'])
@@ -149,6 +207,20 @@ def register_report_web():
     db.session.commit()
     return redirect("/web/patients")
 
+@app.route('/api/labmanager/register',methods=['POST'])
+def insert_Lab_Manager():   
+    lm = db.session.add(Lab_Manager({"name":request.form.get("name"),
+                                "test":request.form.get("test"),
+                                "result":request.form.get("result")}))
+    db.session.commit()
+    labManager = Lab_Manager.query.all()
+    
+    for lm in labManager:
+        print("Id: ",lm.lab_manager_id,"Name: ",lm.name,"Test: ",
+              lm.test,"Result: ",lm.result)
+        
+    return str(labManager)    
+
 @app.route('/api/reports/list')
 def fetch_reports():
     return jsonpickle.encode(Report.query.all())
@@ -157,15 +229,20 @@ def fetch_reports():
 def fetch_patients():
     return jsonpickle.encode(Patient.query.all())
 
+@app.route('/api/labmanager/list')
+def fetch_Lab_Managers():
+    return jsonpickle.encode(Lab_Manager.query.all())
+
 
 if __name__ == '__main__':
     db.create_all()
-    example_Patient()
+#    example_Patient()
+#    example_Lab_Manager()
     example_Report()
+#    example_Lab_Manager()
 #     print("List of Patients in alc_Patients table")
 #     for p in Patient.fetch_all_patients_from_db():
 #         print(p) 
 #     print(Patient.fetch_patient_by_patient_id_from_db(2))
-#    insert_Report()
     app.run(port=7700)
     pass
